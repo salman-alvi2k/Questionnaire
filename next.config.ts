cdd84125dff46c6eb08f181b58e5a4a17467cee2
salman-alvi2k <102091911+salman-alvi2k@users.noreply.github.com>
2024-11-07 18:17:18 +0500

    fix: routing mechanism

--- next.config.ts
+++ next.config.ts
@@ -1,13 +1,6 @@
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
-  redirects: async () => [
-    {
-      source: "/",
-      destination: "/home",
-      permanent: true,
-    },
-  ],
   /* other config options here */
 };
 
